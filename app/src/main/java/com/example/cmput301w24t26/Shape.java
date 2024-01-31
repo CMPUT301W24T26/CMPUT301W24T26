70c2078b7a04843774e86f672cfb0d43603249af
FalsettiM <90410065+FalsettiM@users.noreply.github.com>
2024-01-30 18:15:04 -0700

    Fixed color in Shape

--- app/src/main/java/com/example/cmput301w24t26/Shape.java
+++ app/src/main/java/com/example/cmput301w24t26/Shape.java
@@ -1,7 +1,7 @@
 package com.example.cmput301w24t26;
 
 public abstract class Shape {
-    String color = "$PUT_YOUR_CHOICE_OF_COLOR"
+    String color = "$PUT_YOUR_CHOICE_OF_COLOR";
     
     private int x;
     private int y;
