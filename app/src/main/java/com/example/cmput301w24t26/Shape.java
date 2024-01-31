a16ce89573317ab750501a473e76b5164e126420
FalsettiM <90410065+FalsettiM@users.noreply.github.com>
2024-01-30 18:50:35 -0700

    Cleaned up the file

--- app/src/main/java/com/example/cmput301w24t26/Shape.java
+++ app/src/main/java/com/example/cmput301w24t26/Shape.java
@@ -1,10 +1,7 @@
 package com.example.cmput301w24t26;
 
 public abstract class Shape {
-<<<<<<< HEAD
-=======
     String color = "Blue";
->>>>>>> 39cf133 (Added color to branch)
     
     private int x;
     private int y;
